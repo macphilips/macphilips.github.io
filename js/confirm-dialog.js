ee8be35a44d4d8f55b5bcf9a2fb7bb289e0a1edd
MOROLARI <tmorolari@gmail.com>
2018-07-14 18:03:52 +0200

    Allow confirm dialog to accept custom message

--- js/confirm-dialog.js
+++ js/confirm-dialog.js
@@ -1,7 +1,21 @@
-function ConfirmDeleteEntryView() {
+function ConfirmDeleteEntryView(message) {
     this._viewElement = document.createElement('div');
     this._viewElement.innerHTML = deleteDialogTemple.trim();
     this.actionButtonClicked = new Event(this);
+
+    var model = {message: 'This action delete entry from database. Are you sure you want to continue?'};
+    if (message) {
+        model.message = message;
+    }
+
+    var dataModelElements = this._viewElement.querySelectorAll('[tc-data-model]');
+    var i;
+    for (i = 0; i < dataModelElements.length; i++) {
+        var element = dataModelElements[i];
+        var data = element.getAttribute('tc-data-model');
+        element.innerHTML = getProperty(model, data);
+    }
+
     var okButton = this._viewElement.querySelector('[tc-data-action="ok"]');
     var cancelButton = this._viewElement.querySelector('[tc-data-dismiss="cancel"]');
     var self = this;
